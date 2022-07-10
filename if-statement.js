76a0b434f93b7edfabfb4093db6b8d356097bb85
Jacqline <jacqlinegeng@gmail.com>
2022-07-09 20:35:22 -0400

    Initial commit with all my work so far

---
+++ if-statement.js
@@ -0,0 +1,11 @@
+let fruit = "orange";
+let length = fruit.length;
+
+if (length > 5) {
+	fruit = "The fruit name has more than five characters."
+}
+	else if (length < 5) {
+	fruit = "The fruit name has five characters or less."
+}
+
+console.log(fruit);
---
+++ looping-through-arrays.js
@@ -0,0 +1,8 @@
+const pets = ['cat', 'dog', 'rat']
+var petsLength = pets.length
+
+for (let i = 0; i < petsLength; i++) {
+	pets[i] = pets[i] + 's'
+}
+
+console.log(pets);
